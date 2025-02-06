8d84f4c9989e27efab8763c9828bd6e991e49bef
Muhammad shahzad <shehzadmemon147@gmail.com>
2025-02-06 22:37:21 +0500

    working on signup form

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+import Signup from './android/src/screen/Signup'
+
+const App = () => {
+  return (
+        <Signup/>
+  )
+}
+
+export default App
+
---
+++ android/src/screen/Signup.jsx
@@ -0,0 +1,72 @@
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.headingcontainer}>
+      <Text style={styles.heading}>Signup</Text>
+      </View>
+      <View style={styles.formcontainer}>
+      <TextInput placeholder="Name" style={styles.textinput}/>
+        <TextInput placeholder="Email" style={styles.textinput}/>
+        <TextInput placeholder="Password" style={styles.textinput}/>
+        <TouchableOpacity style={styles.signupbtn}>
+            <Text style={styles.btntext}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+        <View style={styles.extratextcontainer}>
+        <Text>Already have an account?</Text>
+        <TouchableOpacity>
+        <Text>Sign In</Text>
+        </TouchableOpacity>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default Signup
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  headingcontainer:{
+    flex:1,
+    backgroundColor: 'green',
+    
+  },
+  heading:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  formcontainer:{
+    flex:1,
+    backgroundColor: 'green',
+    gap: 10,
+  },  
+  textinput:{
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    padding: 10
+  },
+  signupbtn:{
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    padding: 10
+  },
+  btntext:{
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20  
+  },
+  extratextcontainer:{
+    flex:1,
+    backgroundColor: 'green'
+  },
+
+})
